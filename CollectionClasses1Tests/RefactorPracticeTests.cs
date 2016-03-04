f2cd350ca8841c1095c12dac32d611b5368ecc04
Steve Carroll (VISUAL STUDIO) <scarroll@microsoft.com>
2016-03-03 22:50:49 -0800

    first commit

---
+++ CollectionClasses1Tests/RefactorPracticeTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tests
+{
+    [TestClass()]
+    public class RefactorPracticeTests
+    {
+        [TestMethod()]
+        public void TwoParamsTest()
+        {
+            Assert.Fail();
+            new RefactorPractice();
+
+        }
+    }
+}
